fc9fb1c1d8951fd71ef719091b17927540f09033
Daman kinra <daman.kinra@gmail.com>
2021-11-21 22:03:09 +0530

    cart added

--- src/Product/ProductListDetails.js
+++ src/Product/ProductListDetails.js
@@ -1,39 +1,80 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 const API_KEY = process.env.REACT_APP_API_KEY;
-const ProductListDetails = ({product}) => {
-    console.log(product,"Hello");
-    
-    return (
+const ProductListDetails = ({ product }) => {
+  console.log(product, "Hello");
+  const [user] = useState(
+    localStorage.getItem("user")
+      ? JSON.parse(localStorage.getItem("user"))
+      : null
+  );
+  const addToCart = (prod) => {
+    const old = localStorage.getItem("cart");
+    if (old) {
+      const arr = JSON.parse(old);
+      const newArr = [...arr, prod];
+      localStorage.setItem("cart", JSON.stringify(newArr));
+    } else {
+      const arr = [prod];
+      localStorage.setItem("cart", JSON.stringify(arr));
+    }
+  };
+  return (
+    <div className="col-lg-3 col-md-4 col-sm-12">
+      <div className="card mt-2 h-100">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Product description is yet to be defined</p>
+          <img
+            src={`${API_KEY}` + "/assets/" + `${product.imagefile}`}
+            width="50"
+            height="50"
+            className="d-inline-block align-top img-fluid"
+            alt="No image Avail"
+          />
+          <br />
+          <div className="cardhover">
+            <Link
+              to={`/productDetails/${product._id}`}
+              className="btn btn-primary"
+              data-toggle="tooltip"
+              data-placement="top"
+              title="PRODUCT DETAILS"
+            >
+              <i
+                className="fa fa-plus fa-2x"
+                aria-hidden="true"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="PRODUCT DETAILS"
+              ></i>
+            </Link>
+            {user && (
+              <a
+                //   to={`/cart/add/${product._id}`}
+                className="btn btn-primary ml-4"
+                data-toggle="tooltip"
+                data-placement="top"
+                title="ADD To Cart"
+                onClick={(e) => {
+                  e.preventDefault();
+                  addToCart(product);
+                }}
+              >
+                <i
+                  className="fa fa-cart-plus fa-2x"
+                  aria-hidden="true"
+                  data-toggle="tooltip"
+                  data-placement="top"
+                  title="ADD To CART"
+                ></i>
+              </a>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-        
-            <div className="col-lg-3 col-md-4 col-sm-12">
-                <div className="card mt-2 h-100">
-                    <div className="card-body">
-    
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Product description is yet to be defined</p>
-                        <img src={`${API_KEY}`+'/assets/'+`${product.imagefile}`} width="50" height="50"
-                             className="d-inline-block align-top img-fluid" alt="No image Avail" />
-                        <br />
-                        <div className="cardhover">
-                            <Link to={`/productDetails/${product._id}`} className="btn btn-primary"
-                            data-toggle="tooltip" data-placement="top" title="PRODUCT DETAILS">
-                                <i className="fa fa-plus fa-2x" aria-hidden="true" data-toggle="tooltip"
-                                data-placement="top" title="PRODUCT DETAILS"></i>
-                            </Link>
-                            <Link to={`/cart/add/${product._id}`} className="btn btn-primary ml-4"
-                            data-toggle="tooltip" data-placement="top" title="ADD To Cart">
-                                <i className="fa fa-cart-plus fa-2x" aria-hidden="true" data-toggle="tooltip"
-                                data-placement="top" title="ADD To CART"></i>
-                            </Link>
-                            </div>     
-                    </div>
-                </div>
-    
-            </div>
-        )
-    
-}
-
-export default ProductListDetails
+export default ProductListDetails;
